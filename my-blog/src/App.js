232c7e2a969d7d522058b75c7cbc34f98e1c2fc5
sushmita4296 <sushmitaprity@40gmail.com>
2022-03-31 18:34:24 +0600

    progress header

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Home />
     </div>
   );
 }
---
+++ my-blog/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header/header";
+import "./home.css"
+
+function Home(){
+  return(
+    <div className="home">
+      <Header />
+      <h1>Homepage</h1>
+    </div>
+  )
+}
+export default Home;
