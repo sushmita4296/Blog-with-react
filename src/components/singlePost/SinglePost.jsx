ec552b9c74877afdc1e266ea8ec05e98afc31785
sushmita4296 <sushmitaprity@gmail.com>
2022-04-26 21:33:26 +0600

    update singlePost

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,9 +1,26 @@
 import "./singlePost.css"
+import  pImg1 from "../../images/pImg1.jpg"
 
 function SinglePost(){
     return(
         <div className="singlePost">
-            singlepost
+            <div className="singlePostWrapper">
+            <img className="singlePostImg" src={pImg1} alt="post-image" />
+            
+            <h1 className="singlePostTitle">
+                Loream Ipsum doler sit 
+                <div className="singlePostEdit">
+                    <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+                    <i className="singlePostIcon fa-solid fa-trash-can"></i>
+                </div>
+            </h1>
+            <div className="singlePostInfo">
+                    <span className="singlePostAuthor">Author: <b>Sushmita</b> </span>
+                    <span className="singlePostDate">1 hour ago</span>
+                </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur facilis adipisci esse consectetur beatae illo veritatis dolore vitae, laboriosam libero pariatur magnam repellendus nihil quidem blanditiis vel laudantium soluta eveniet.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur facilis adipisci esse consectetur beatae illo veritatis dolore vitae, laboriosam libero pariatur magnam repellendus nihil quidem blanditiis vel laudantium soluta eveniet.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur facilis adipisci esse consectetur beatae illo veritatis dolore vitae, laboriosam libero pariatur magnam repellendus nihil quidem blanditiis vel laudantium soluta eveniet.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur facilis adipisci esse consectetur beatae illo veritatis dolore vitae, laboriosam libero pariatur magnam repellendus nihil quidem blanditiis vel laudantium soluta eveniet.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur facilis adipisci esse consectetur beatae illo veritatis dolore vitae, laboriosam libero pariatur magnam repellendus nihil quidem blanditiis vel laudantium soluta eveniet.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur facilis adipisci esse consectetur beatae illo veritatis dolore vitae, laboriosam libero pariatur magnam repellendus nihil quidem blanditiis vel laudantium soluta eveniet.</p>
+
+            </div>
         </div>
     );
 }
