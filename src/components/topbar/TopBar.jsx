742bb6792473b157bb4a6cdd05f46e93faf59d19
sushmita4296 <sushmitaprity@gmail.com>
2022-05-24 01:11:29 +0600

    update new change in topbar

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -26,19 +26,29 @@ const TopBar = () => {
         <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div> */}
 
-      <div className="topMenu">
+      <div className="topMenu container">
         <nav className="navbar" role="navigation" aria-label="main navigation">
-          <a
-            role="button"
-            class="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
+          <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+              <img
+                src="https://bulma.io/images/bulma-logo.png"
+                width="112"
+                height="28"
+              />
+            </a>
+
+            <a
+              role="button"
+              class="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
           <div id="navbarBasicExample" className="navbar-menu">
             <div className="navbar-start">
               <a className="navbar-item">Home</a>
